1b992bcbe05f1edb046e6a8bf4df6ca22a25e747
bill <beiller@gmail.com>
2018-01-27 18:10:13 -0500

    Setup utils

---
+++ examples/mt_order_entry/test.py
@@ -0,0 +1,48 @@
+import liquibook
+import random
+import time
+import uuid
+import json
+
+def my_fucking_callback(code, data):
+	print(code, json.loads(data.strip()))
+
+liquibook.set_callback(my_fucking_callback)
+
+def generate_order_id():
+	return str(uuid.uuid4())
+
+def generate_random_command():
+	return "{} {} !IBM {} ;".format(
+		['BUY', 'SELL'][random.randint(0,1)],
+		random.randint(5,1000),
+		[random.randint(40,70), 'MKT'][random.randint(0,1)]
+	)
+	#if n == 1:
+	#	return "CANCEL #{} ;".format()
+
+def random_test():
+	start_time = time.time()
+	while True:
+
+		myid = generate_order_id()
+		mycommand = generate_random_command()
+		liquibook.foo(myid, mycommand)
+
+def controlled_test():
+	script = """BUY 100.00000001 !XMR/BTC 0.00010001 ;
+SELL 100.00000001 !XMR/BTC 0.00010001 ;
+"""
+	for line in script.split('\n'):
+		order_id = generate_order_id()
+		liquibook.foo(order_id, line.strip())
+
+
+def modify_test():
+	my_id = generate_order_id()
+	liquibook.foo(my_id, 'BUY 100 !IBM 49 ;')
+	liquibook.foo(my_id, 'MODIFY #{} PRICE 101 QUANTITY -1 ;'.format(my_id))
+	liquibook.foo(my_id, 'CANCEL #{} ;'.format(my_id))
+		
+
+random_test()
