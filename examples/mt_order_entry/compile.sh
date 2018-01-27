d8dfb204777d31ff6d6b3a4258a676177e03c6eb
bill <beiller@gmail.com>
2018-01-27 16:07:05 -0500

    Add some more type fixes and int cleaning. Added an Epsilon

--- examples/mt_order_entry/compile.sh
+++ examples/mt_order_entry/compile.sh
@@ -1,4 +1,4 @@
-clang++ -fPIC -shared -undefined dynamic_lookup Market.cpp Order.cpp Util.cpp liquibookmodule.cpp \
+clang++ -Wall -fPIC -shared -undefined dynamic_lookup Market.cpp Order.cpp Util.cpp liquibookmodule.cpp \
 -o liquibook.so --include-directory=../../src/ -std=c++14 \
 -I/Library/Frameworks/Python.framework/Versions/3.6/include/python3.6m/ \
 -L/Library/Frameworks/Python.framework/Versions/3.6/lib/ \
